cbc37ee2329411ceccb9d010958c5a272104698f
Kento Shigyo <kentoglwo@gmail.com>
2021-01-01 16:23:51 +0800

    :sparkles: add Top

---
+++ src/components/top/Top.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../assets/styles/Top.scss";
+
+interface Props {}
+interface State {}
+
+class Top extends React.Component<Props, State> {
+  public render() {
+    return (
+      <div id="top">
+        <h2>top page</h2>
+      </div>
+    );
+  }
+}
+
+export default Top;
